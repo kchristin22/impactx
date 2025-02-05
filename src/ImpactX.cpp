3659c7c528cb10cdd9b58344c57589ba2ca3f10e
Axel Huebl <axel.huebl@plasma.ninja>
2025-02-04 23:39:02 -0800

    [Work-Around] Avoid Variants, Push Manually
    Work-around for
    https://github.com/EnzymeAD/Enzyme/issues/2235#issuecomment-2635852749

--- src/ImpactX.cpp
+++ src/ImpactX.cpp
@@ -157,6 +157,14 @@ namespace impactx {
 
         // ns = 10  // TODO: number of slices per ds in the element
 
+        auto dr1 = Drift{2.7};
+        auto q1 = Quad{0.1, q1_k};
+        auto dr2 = Drift{1.4};
+        auto q2 = Quad{0.2, q2_k};
+        auto dr3 = Drift{1.4};
+        auto q3 = Quad{0.1, q1_k};
+        auto dr4 = Drift{2.7};
+        /*
         // quadrupole triplet
         // https://impactx.readthedocs.io/en/latest/usage/examples/optimize_triplet/README.html
         active_sim->m_lattice = {
@@ -168,13 +176,18 @@ namespace impactx {
             Quad{0.1, q1_k},
             Drift{2.7}
         };
-        /*
+
 
         // int ns = 25;  // number of slices per ds in the element
         // for (auto & e : sim.m_lattice) { e.m_nslice = ns; }
+        */
 
-        active_sim->evolve();
+        //active_sim->evolve();
+        auto & pc = *active_sim->amr_data->m_particle_container;
 
+        dr1(pc, 0, 0);
+
+        /*
         std::unordered_map<std::string, amrex::ParticleReal> const rbc =
                 diagnostics::reduced_beam_characteristics(*active_sim->amr_data->m_particle_container);
 
